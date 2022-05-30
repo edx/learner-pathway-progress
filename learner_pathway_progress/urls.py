0c4e0de0bbe142fc643af2f67523e0908ceb12d6
Muneeb Ur Rehman <muneeb.github@gmail.com>
2022-05-30 18:02:25 +0500

    refactor: remove unused imports

--- learner_pathway_progress/urls.py
+++ learner_pathway_progress/urls.py
@@ -1,8 +1,6 @@
 """
 URLs for learner_pathway_progress.
 """
-from django.conf.urls import url  # pylint: disable=unused-import
-from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
