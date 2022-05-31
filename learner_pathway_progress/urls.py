df83ed8cd961bfdaf90cd4db9edf8a74370d3df4
Muneeb Ur Rehman <muneeb.github@gmail.com>
2022-05-31 12:53:48 +0500

    feat: remove check_reserved_keywords action

--- learner_pathway_progress/urls.py
+++ learner_pathway_progress/urls.py
@@ -1,6 +1,8 @@
 """
 URLs for learner_pathway_progress.
 """
+from django.urls import re_path  # pylint: disable=unused-import
+from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
